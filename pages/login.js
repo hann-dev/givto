070bb10a22c85d6d907926389875126ac34adae2
hann-dev <mhannav@hotmail.com>
2020-05-04 22:28:22 +0100

    login

--- pages/login.js
+++ pages/login.js
@@ -41,30 +41,13 @@ class Login extends Component {
                   Username
                 </label>
 
-                <p className="control has-icons-left">
+                <p className="control">
                   <input
                     className="input"
                     type="username"
                     placeholder="Username"
                   />
-                  <span className="icon is-small is-left iusername">
-                    <svg
-                      className="svg-inline--fa fa-user fa-w-16"
-                      aria-hidden="true"
-                      data-prefix="fas"
-                      data-icon="user"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                      data-fa-i2svg=""
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M256 0c88.366 0 160 71.634 160 160s-71.634 160-160 160S96 248.366 96 160 167.634 0 256 0zm183.283 333.821l-71.313-17.828c-74.923 53.89-165.738 41.864-223.94 0l-71.313 17.828C29.981 344.505 0 382.903 0 426.955V464c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48v-37.045c0-44.052-29.981-82.45-72.717-93.134z"
-                      ></path>
-                    </svg>
-                    <i classxx="fas fa-user"></i>
-                  </span>
+                  
                 </p>
               </div>
 
@@ -73,30 +56,13 @@ class Login extends Component {
                   Password
                 </label>
 
-                <p className="control has-icons-left">
+                <p className="control">
                   <input
                     className="input"
                     type="password"
                     placeholder="Password"
                   />
-                  <span className="icon is-small is-left ipassword">
-                    <svg
-                      className="svg-inline--fa fa-lock fa-w-14"
-                      aria-hidden="true"
-                      data-prefix="fas"
-                      data-icon="lock"
-                      role="img"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                      data-fa-i2svg=""
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"
-                      ></path>
-                    </svg>
-                    <i classxxx="fas fa-lock"></i>
-                  </span>
+                
                 </p>
               </div>
               <div className="field">
